1703367b9f51251bc3969441739804bb3daf1f04
Bertrand Martin <bertrand@sentrysoftware.com>
2024-09-22 22:21:53 +0200

    Proper handling of file version

---
+++ version.h
@@ -0,0 +1,13 @@
+// version.h
+#define VERSION_MAJOR 2
+#define VERSION_MINOR 0
+#define VERSION_PATCH 0
+#define VERSION_BUILD 0
+
+#define VERSION_STR "2.0.00"
+
+#define COPYRIGHT "Copyright (C) 2024 Sentry Software"
+
+#ifndef EXECUTABLE_NAME
+#define EXECUTABLE_NAME "EventLogReader"
+#endif
